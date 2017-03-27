e00c999e6fc59e901c3afcf590d0f33753c657d9
relucent <relucent@163.com>
2017-03-27 12:55:51 +0800

    +

---
+++ src/main/java/yyl/spring/boot/controller/JspController.java
@@ -0,0 +1,22 @@
+package yyl.spring.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * |~ http://localhost:8080/jsp/welcome
+ * @deprecated
+ */
+@Controller
+@RequestMapping("/jsp")
+public class JspController {
+
+	//|~ /jsp/welcome
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String welcome(Model mv) {
+		mv.addAttribute("message", "hello jsp");
+		return "hello/welcome";
+	}
+}
