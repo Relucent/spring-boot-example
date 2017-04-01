b623ed5e80caadb15c6a6b8bc115ad6348c303cc
relucent <relucent@163.com>
2017-04-01 15:45:59 +0800

    +

--- src/main/java/yyl/spring/boot/Application.java
+++ src/main/java/yyl/spring/boot/Application.java
@@ -2,13 +2,22 @@ package yyl.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 /**
  * &#64;SpringBootApplication 等同于 &#64;Configuration | &#64;ComponentScan | &#64;EnableAutoConfiguration
  */
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+	// ==============================[Override]=======================================
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	// ==============================[Run]============================================
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("start-up");
