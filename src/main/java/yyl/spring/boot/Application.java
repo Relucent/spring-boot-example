9b363938c29105cb244707f57b07457880e9c7cb
relucent <relucent@163.com>
2018-01-11 17:07:19 +0800

    #

--- src/main/java/yyl/spring/boot/Application.java
+++ src/main/java/yyl/spring/boot/Application.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 /**
  * &#64;SpringBootApplication 等同于 &#64;Configuration | &#64;ComponentScan | &#64;EnableAutoConfiguration
  */
+@EnableAsync
 public class Application extends SpringBootServletInitializer {
 
 	// ==============================[Override]=======================================
--- src/main/java/yyl/spring/boot/controller/HelloController.java
+++ src/main/java/yyl/spring/boot/controller/HelloController.java
@@ -1,11 +1,15 @@
 package yyl.spring.boot.controller;
 
+import java.util.concurrent.Future;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import yyl.spring.boot.model.Hello;
+import yyl.spring.boot.service.HelloService;
 
 /**
  * |~ http://localhost:8080/hello
@@ -14,14 +18,26 @@ import yyl.spring.boot.model.Hello;
 @RequestMapping("/hello")
 public class HelloController {
 
+	@Autowired
+	private HelloService helloService;
+
 	//|~ /hello/1
 	@RequestMapping(value = "/{id}")
 	@ResponseBody
 	public Object view(@PathVariable("id") Long id) {
-		Hello model = new Hello();
-		model.setId(id);
-		model.setName("spring boot");
+		Hello model = helloService.getById(id);
 		return model;
 	}
 
+	//|~ /hello/async
+	@RequestMapping(value = "/async")
+	@ResponseBody
+	public Object async() {
+		try {
+			helloService.asyncTask();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return System.currentTimeMillis();
+	}
 }
