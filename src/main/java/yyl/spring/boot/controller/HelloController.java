2df5344efe762488d7fc7feb24807c45d70ad01f
relucent <relucent@163.com>
2018-01-11 17:07:35 +0800

    #

--- src/main/java/yyl/spring/boot/controller/HelloController.java
+++ src/main/java/yyl/spring/boot/controller/HelloController.java
@@ -1,7 +1,5 @@
 package yyl.spring.boot.controller;
 
-import java.util.concurrent.Future;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
