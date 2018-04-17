70919e363534ba182ebceeb49f0d4d47dabad9ec
relucent <relucent@163.com>
2018-04-17 09:34:06 +0800

    #

--- spring-boot-shiro/src/main/java/yyl/springboot/controller/IndexController.java
+++ spring-boot-shiro/src/main/java/yyl/springboot/controller/IndexController.java
@@ -2,9 +2,11 @@ package yyl.springboot.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,8 @@ public class IndexController {
 
 	@RequestMapping("/login")
 	public String login(HttpServletRequest request, Model model) throws Exception {
-		System.out.println("HomeController.login()");
+		
+		System.out.println("/login");
 
 		// 登录失败从request中获取shiro处理的异常信息。
 		// shiroLoginFailure:就是shiro异常类的全类名.
@@ -41,27 +44,33 @@ public class IndexController {
 				System.out.println("else -- >" + exception);
 			}
 		}
-
 		model.addAttribute("msg", msg);
-		return "/login";
+		Subject subject = SecurityUtils.getSubject();
+		if (subject.isAuthenticated()) {
+			return "redirect:/index";
+		}
+		return "login";
 	}
 
 	@RequiresPermissions("permission:page1")
 	@RequestMapping("/page1")
 	public String page1() {
-		return "/page1";
+		
+		System.out.println("HomeController.page1()");
+		
+		return "page1";
 	}
 
 	// @RequiresPermissions("permission:page2")
 	@RequestMapping("/page2")
 	public String page2() {
-		return "/page2";
+		return "page2";
 	}
 
 	@RequiresPermissions("permission:page3")
 	@RequestMapping("/page3")
 	public String page3() {
-		return "/page3";
+		return "page3";
 	}
 
 	@RequestMapping("/403")
