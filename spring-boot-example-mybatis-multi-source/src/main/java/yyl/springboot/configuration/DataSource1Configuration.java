4c1265f42e584b383887d0786d576f883a209119
yyl <relucent@163.com>
2018-09-06 17:44:27 +0800

    #

--- spring-boot-example-mybatis-multi-source/src/main/java/yyl/springboot/configuration/DataSource1Configuration.java
+++ spring-boot-example-mybatis-multi-source/src/main/java/yyl/springboot/configuration/DataSource1Configuration.java
@@ -12,6 +12,8 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
@@ -21,36 +23,39 @@ import yyl.springboot.annotation.MapperRepository;
 @MapperScan(basePackages = "yyl.springboot.mapper.db1", sqlSessionTemplateRef = "sqlSessionTemplate1", annotationClass = MapperRepository.class)
 public class DataSource1Configuration {
 
-	@Bean(name = "dataSource1")
-	@ConfigurationProperties(prefix = "spring.datasource.db1")
-	@Primary
-	public DataSource dataSource1() {
-		return DataSourceBuilder.create().build();
-	}
+    @Bean(name = "dataSource1")
+    @ConfigurationProperties(prefix = "spring.datasource.db1")
+    @Primary
+    public DataSource dataSource1() {
+        return DataSourceBuilder.create().build();
+    }
 
-	@Bean(name = "sqlSessionFactory1")
-	@Primary
-	public SqlSessionFactory sqlSessionFactory1(@Qualifier("dataSource1") DataSource dataSource) throws Exception {
-		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-		bean.setDataSource(dataSource);
-		return bean.getObject();
-	}
+    @Bean(name = "sqlSessionFactory1")
+    @Primary
+    public SqlSessionFactory sqlSessionFactory1(@Qualifier("dataSource1") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        bean.setMapperLocations(resolver.getResources("classpath:yyl/springboot/mapper/db1/*Mapper.xml"));
+        return bean.getObject();
+    }
 
-	@Bean(name = "transactionManager1")
-	@Primary
-	public DataSourceTransactionManager transactionManager1(@Qualifier("dataSource1") DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource);
-	}
+    @Bean(name = "transactionManager1")
+    @Primary
+    public DataSourceTransactionManager transactionManager1(@Qualifier("dataSource1") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
 
-	@Bean(name = "sqlSessionTemplate1")
-	@Primary
-	public SqlSessionTemplate sqlSessionTemplate1(@Qualifier("sqlSessionFactory1") SqlSessionFactory sqlSessionFactory) throws Exception {
-		return new SqlSessionTemplate(sqlSessionFactory);
-	}
+    @Bean(name = "sqlSessionTemplate1")
+    @Primary
+    public SqlSessionTemplate sqlSessionTemplate1(@Qualifier("sqlSessionFactory1") SqlSessionFactory sqlSessionFactory)
+            throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
 
-	@Bean(name = "jdbcTemplate1")
-	@Primary
-	public JdbcTemplate jdbcTemplate1() {
-		return new JdbcTemplate(dataSource1());
-	}
+    @Bean(name = "jdbcTemplate1")
+    @Primary
+    public JdbcTemplate jdbcTemplate1() {
+        return new JdbcTemplate(dataSource1());
+    }
 }
--- spring-boot-example-mybatis-multi-source/src/main/java/yyl/springboot/configuration/DataSource2Configuration.java
+++ spring-boot-example-mybatis-multi-source/src/main/java/yyl/springboot/configuration/DataSource2Configuration.java
@@ -11,6 +11,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
@@ -20,31 +22,34 @@ import yyl.springboot.annotation.MapperRepository;
 @MapperScan(basePackages = "yyl.springboot.mapper.db2", sqlSessionTemplateRef = "sqlSessionTemplate2", annotationClass = MapperRepository.class)
 public class DataSource2Configuration {
 
-	@Bean(name = "dataSource2")
-	@ConfigurationProperties(prefix = "spring.datasource.db2")
-	public DataSource dataSource2() {
-		return DataSourceBuilder.create().build();
-	}
+    @Bean(name = "dataSource2")
+    @ConfigurationProperties(prefix = "spring.datasource.db2")
+    public DataSource dataSource2() {
+        return DataSourceBuilder.create().build();
+    }
 
-	@Bean(name = "sqlSessionFactory2")
-	public SqlSessionFactory sqlSessionFactory2(@Qualifier("dataSource2") DataSource dataSource) throws Exception {
-		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-		bean.setDataSource(dataSource);
-		return bean.getObject();
-	}
+    @Bean(name = "sqlSessionFactory2")
+    public SqlSessionFactory sqlSessionFactory2(@Qualifier("dataSource2") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        bean.setMapperLocations(resolver.getResources("classpath:yyl/springboot/mapper/db2/*Mapper.xml"));
+        return bean.getObject();
+    }
 
-	@Bean(name = "transactionManager2")
-	public DataSourceTransactionManager transactionManager2(@Qualifier("dataSource2") DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource);
-	}
+    @Bean(name = "transactionManager2")
+    public DataSourceTransactionManager transactionManager2(@Qualifier("dataSource2") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
 
-	@Bean(name = "sqlSessionTemplate2")
-	public SqlSessionTemplate sqlSessionTemplate2(@Qualifier("sqlSessionFactory2") SqlSessionFactory sqlSessionFactory) throws Exception {
-		return new SqlSessionTemplate(sqlSessionFactory);
-	}
+    @Bean(name = "sqlSessionTemplate2")
+    public SqlSessionTemplate sqlSessionTemplate2(@Qualifier("sqlSessionFactory2") SqlSessionFactory sqlSessionFactory)
+            throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
 
-	@Bean(name = "jdbcTemplate2")
-	public JdbcTemplate jdbcTemplate2() {
-		return new JdbcTemplate(dataSource2());
-	}
+    @Bean(name = "jdbcTemplate2")
+    public JdbcTemplate jdbcTemplate2() {
+        return new JdbcTemplate(dataSource2());
+    }
 }
